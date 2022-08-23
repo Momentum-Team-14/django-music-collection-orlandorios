a6b6a4825ed9bfaff97345a89dfef2bd463ba35e
Orlando <carlos.orlando.rios@gmail.com>
2022-08-23 09:18:16 -0500

    set urls to link to html

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
