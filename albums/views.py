5b35b820c6ab43c5448a14b925efb395ee970169
Orlando <carlos.orlando.rios@gmail.com>
2022-08-24 10:13:35 -0500

    made a create an album form

--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from django.shortcuts import render, get_object_or_404
 from .models import Album
+from .forms import AlbumForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def album_list(request):
     albums = Album.objects.all()
     return render(request, 'albums/album_list.html', {'albums': albums})
+
+def album_detail(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    return render(request, 'albums/album_detail.html', {'album': album})
+
+def create_album(request):
+    if request.method == "POST":
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            album = form.save()
+            return redirect('album_list')
+    else:
+        form = AlbumForm()
+    return render (request, 'albums/create_album.html', {'form':form})
