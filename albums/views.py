d66abbe17285ff2c7e28eb5f4fef2a9e047a69b8
Orlando <carlos.orlando.rios@gmail.com>
2022-08-25 09:18:52 -0500

    added edit artist/album function and link

--- albums/views.py
+++ albums/views.py
@@ -22,3 +22,14 @@ def create_album(request):
     else:
         form = AlbumForm()
     return render (request, 'albums/create_album.html', {'form':form})
+
+def album_edit(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            album = form.save()
+            return redirect('album_detail', pk=album.pk)
+    else:
+        form = AlbumForm(instance=album)
+    return render(request, 'albums/album_edit.html', {'form': form})
